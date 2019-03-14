78f3c4fd8a90dc4cfb1e229a78853562583944b0
Thomas <McSerbik@gmail.com>
2019-03-14 23:34:39 +0200

    onePointOne

---
+++ console/migrations/m170831_002914_CreateRoles.php
@@ -0,0 +1,54 @@
+<?php
+
+use yii\db\Migration;
+
+class m170831_002914_CreateRoles extends Migration
+{
+    public function safeUp()
+    {
+        $rbac = Yii::$app->authManager;
+
+        $admin = $rbac->createRole('admin');
+        $admin->description = 'Can do anything including managing users';
+        $rbac->add($admin);
+
+
+        $manager = $rbac->createRole('manager');
+        $manager->description = 'Can do anything including managing users';
+        $manager->type = 10;
+
+        $rbac->add($manager);
+
+        $guest = $rbac->createRole('guest');
+        $guest->description = 'Only read';
+        $rbac->add($guest);
+
+        $rbac->addChild($manager, $guest);
+        $rbac->addChild($admin, $manager);
+
+
+    }
+
+    public function safeDown()
+    {
+        $manager = Yii::$app->authManager;
+        $manager->removeAll();
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170831_002914_CreateRoles cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
